d4c515a7b38777d9320bcfceaf9421711f03c3b3
Jason Boxman <jasonb@edseek.com>
2013-02-27 20:25:11 -0500

    Checkpoint.

--- lib/corporeal/task.rb
+++ lib/corporeal/task.rb
@@ -7,6 +7,11 @@ require 'corporeal/action'
 require 'corporeal/template'
 require 'corporeal/util'
 
+#
+# http://stackoverflow.com/questions/5729071
+# /how-to-compose-thor-tasks-in-separate-classes-modules-files
+#
+
 require 'corporeal/task/base'
 require 'corporeal/task/distro'
 require 'corporeal/task/profile'
